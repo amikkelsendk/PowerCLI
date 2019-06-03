7b2d3492d08f4766387c4d7bc317aa0274f86d1d
Anders Mikkelsen <35070042+amikkelsendk@users.noreply.github.com>
2019-06-03 14:04:47 +0200

    Update Functions-ContentLibrary.ps1

--- Functions-ContentLibrary.ps1
+++ Functions-ContentLibrary.ps1
@@ -615,16 +615,67 @@ function func_Add-TemplateToLibrary {
     }
 }
 
+Function Remove-ContentLibraryItems {
+    <#
+        .NOTES
+        ===========================================================================
+        Created by:    Anders Mikkelsen
+        Organization:  net IT Services
+        GitHub:        https://github.com/amikkelsendk
+        Twitter:       @AMikkelsenDK
+        ===========================================================================
+        .DESCRIPTION
+            This function deletes a Content Library Item
+        .PARAMETER LibraryName
+            The name of a vSphere Content Library
+        .PARAMETER LibraryItemName
+            The name of a vSphere Content Library Item
+        .EXAMPLE
+            Remove-ContentLibraryItems -LibraryName Test
+        .EXAMPLE
+            Remove-ContentLibraryItems -LibraryName Test -LibraryItemName RemoveMe
+    #>
+    param(
+        [Parameter(Mandatory=$true)][String]$LibraryName,
+        [Parameter(Mandatory=$false)][String]$LibraryItemName
+    )
+
+    $contentLibraryService = Get-CisService com.vmware.content.library
+    $LibraryIDs = $contentLibraryService.list()
+
+    foreach($libraryID in $LibraryIDs) {
+        $library = $contentLibraryService.get($libraryId)
+        if($library.name -eq $LibraryName) {
+            $contentLibraryItemService = Get-CisService com.vmware.content.library.item
+            $itemIds = $contentLibraryItemService.list($libraryID)
+
+            foreach($itemId in $itemIds) {
+                $item = $contentLibraryItemService.get($itemId)
+
+                if($LibraryItemName -eq $item.name) {
+                    #Perform Delete
+                    try {
+                        Write-Host -ForegroundColor Cyan "Deleting library item: " $item.Name "..."
+                        $deleteResult = $contentLibraryItemService.delete($item.Id)
+                    } catch {
+                        Write-Host -ForegroundColor Red "Failed to delete library item:" $item.Name
+                        $Error[0]
+                        break
+                    }
+                }
+            }
+        }
+    }
+}
+
 
 ## Connect to vSphere Automation SDK
 Connect-VIServer -Server 192.168.1.119 -User "administrator@vsphere.local" -Password "VMware1!" | Out-Null
 Connect-CisServer -Server 192.168.1.119 -User "administrator@vsphere.local" -Password "VMware1!" | Out-Null
 
     ## A.Mikkelsen ##
-    
-    # OVF Template
     #func_Add-TemplateToLibrary -LibraryName 'Pod200' -VMname 'contenttest' -LibItemName 'CentOS7 Template 2' -Description 'Uploaded via PowerShell'
-
+    #Remove-ContentLibraryItems -LibraryName 'Pod200' -LibraryItemName 'DeleteMe'
 
     ## LAMW ##
     #Get-ContentLibrary
