cd5d20b76a83151a76d8736b86a79d1064bbf232
ami <am@amikkelsen.com>
2021-02-08 16:21:30 +0100

    Howto add colored checkmarks to powershell output

---
+++ colored-checkmarks.ps1
@@ -0,0 +1,24 @@
+﻿$greenCheck = @{
+  Object = [Char]8730
+  ForegroundColor = 'Green'
+  NoNewLine = $true
+  }
+
+$redCheck = @{
+  Object = [Char]8730
+  ForegroundColor = 'Red'
+  NoNewLine = $true
+  }
+
+
+Write-Host "Status check... " -NoNewline
+Start-Sleep -Seconds 1
+Write-Host @greenCheck
+Write-Host " (Done)"
+
+write-host ""
+
+Write-Host "Status check... " -NoNewline
+Start-Sleep -Seconds 1
+Write-Host @redCheck
+Write-Host " (Error)"
