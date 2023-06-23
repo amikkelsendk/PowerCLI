13c96eab9d6516da357f7db4176e4cce60c31e16
Anders Mikkelsen (AMI) <am@amikkelsen.com>
2023-06-23 11:30:51 +0200

    Added  Nested HashTables

--- multidimensional_array.ps1
+++ multidimensional_array.ps1
@@ -11,25 +11,38 @@
   website:	      www.amikkelsen.com
   Author:         Anders Mikkelsen
   Creation Date:  2019-02-25
-  
+  Updated Date:   2023-06.23
   Known bugs:
 	n/a
 .EXAMPLE
   N/A
 #>
 
-CLS
+Clear-Host
 
+# HashTable
 ## Pre Powershell version 3.0
 $arrMulti = @(
     New-Object PSObject -Property @{Name = "Dennis";  City = "New York"; Size = "M"}
     New-Object PSObject -Property @{Name = "Kenneth"; City = "Chicago"; Size = "S"}
 )
-#write $arrMulti
+$arrMulti
 
 ## Powershell version 3.0 and newer
 $arrMultiNew = @(
     [PSCustomObject]@{Name = "Dennis";  City = "New York"; Size = "M"}
     [PSCustomObject]@{Name = "Kenneth"; City = "Chicago"; Size = "S"}
 )
-write $arrMultiNew
+$arrMultiNew
+
+
+# Nested HashTables
+$arrNestedHashTables = @{}
+$arrNestedHashTables["Cities"] = @{}
+$arrNestedHashTables["Cities"]["Chicago"] = @{ Country = "USA"; Population = "8 mil" }
+$arrNestedHashTables["Cities"]["New York"] = @{ Country = "USA"; Population = "15 mil" }
+
+#$arrNestedHashTables
+#$arrNestedHashTables.Cities
+#$arrNestedHashTables.Cities.Chicago
+#$arrNestedHashTables.Cities.Chicago.Country
