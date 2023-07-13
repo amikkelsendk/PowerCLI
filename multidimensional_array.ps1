118d7c1e5901c1c5f352f4066e9de2dce4beb478
Anders Mikkelsen (AMI) <am@amikkelsen.com>
2023-07-13 22:32:04 +0200

    Updated with examples and ListOfDicts

--- multidimensional_array.ps1
+++ multidimensional_array.ps1
@@ -11,7 +11,7 @@
   website:	      www.amikkelsen.com
   Author:         Anders Mikkelsen
   Creation Date:  2019-02-25
-  Updated Date:   2023-06.23
+  Updated Date:   2023-07-13
   Known bugs:
 	n/a
 .EXAMPLE
@@ -20,7 +20,7 @@
 
 Clear-Host
 
-# HashTable
+# HashTable ( Lists of Dictionarys )
 ## Pre Powershell version 3.0
 $arrMulti = @(
     New-Object PSObject -Property @{Name = "Dennis";  City = "New York"; Size = "M"}
@@ -33,8 +33,28 @@ $arrMultiNew = @(
     [PSCustomObject]@{Name = "Dennis";  City = "New York"; Size = "M"}
     [PSCustomObject]@{Name = "Kenneth"; City = "Chicago"; Size = "S"}
 )
+# or
+$arrMultiNew = @(
+    @{Name = "Dennis";  City = "New York"; Size = "M"}
+    @{Name = "Kenneth"; City = "Chicago"; Size = "S"}
+)
 $arrMultiNew
 
+# Example usage:
+$arrMultiNew[0]
+$arrMultiNew | Select-Object Name
+$arrMultiNew | Where-Object { $_.Name -eq "Dennis" }
+$arrMultiNew | ForEach-Object {  $_.Size = "Medium" }
+$arrMultiNew | Where-Object { $_.Size -eq "M" } | ForEach-Object{  $_.Size = "Medium" }
+$arrMultiNew += @{Name = "Chad";  City = "LA"; Size = "XL"}
+( $arrMultiNew | Where-Object { $_.Name -eq "Dennis" } )["NewKey"] = "NewValue"
+( $arrMultiNew | Where-Object { $_.Name -eq "Dennis" } ).Remove("NewKey")
+$arrMultiNew | Get-Member
+$arrMultiNew.ContainsKey("Dennis")
+
+$arrMultiNew | ConvertTo-Json
+
+##########################################################################################
 
 # Nested HashTables
 $arrNestedHashTables = @{}
@@ -42,7 +62,19 @@ $arrNestedHashTables["Cities"] = @{}
 $arrNestedHashTables["Cities"]["Chicago"] = @{ Country = "USA"; Population = "8 mil" }
 $arrNestedHashTables["Cities"]["New York"] = @{ Country = "USA"; Population = "15 mil" }
 
-#$arrNestedHashTables
-#$arrNestedHashTables.Cities
-#$arrNestedHashTables.Cities.Chicago
-#$arrNestedHashTables.Cities.Chicago.Country
+# Example usage:
+$arrNestedHashTables
+$arrNestedHashTables.Cities
+$arrNestedHashTables.Cities.Chicago | gm
+$arrNestedHashTables.Cities.Chicago.Country
+$arrNestedHashTables.Cities | Get-Member
+$arrNestedHashTables.Cities | %{ $_ }
+( $arrNestedHashTables.Cities ).Keys -eq "Chicago"
+( $arrNestedHashTables.Cities ).Contains("Chicago")
+( $arrNestedHashTables.Cities ).ContainsKey("Chicago")
+$arrNestedHashTables.Cities.Chicago.Country = "US"
+$arrNestedHashTables["Cities"]["Chicago"]["Country"] = "USA"
+$arrNestedHashTables["Cities"] = @{}
+($arrNestedHashTables.Cities).Remove("Chicago")
+
+$arrNestedHashTables | ConvertTo-Json
