bf53c1152a7cf425f6e4fe18f1339924556c9ce5
Anders Mikkelsen (AMI) <am@amikkelsen.com>
2024-04-24 12:11:54 +0200

    Updated with attachments

--- function_send_mail.ps1
+++ function_send_mail.ps1
@@ -5,6 +5,7 @@ Function Send-EMail {
         Created by:    Anders Mikkelsen
         Twitter:       @AMikkelsenDK
         Creation Date: 2022-03-21
+        Modified Date: 2024-04-24
         ===========================================================================
     .DESCRIPTION
         This function sendsan e-mail
@@ -18,8 +19,74 @@ Function Send-EMail {
         E-Mail body
     .PARAMETER smtpServer
         SMTP server FQDN or IP
+    .PARAMETER Attachment
+        Full path to attachment
+        Only one attachment supported
     .EXAMPLE
+        Without Att.:
         Send-EMail -To "to@email.com" -From "from@email.com" -Subject "HEY" -Body "TEST Mail" -smtpServer "relay.email.com"
+        With Att.:
+        Send-EMail -To "to@email.com" -From "from@email.com" -Subject "HEY" -Body "TEST Mail" -Attachment "c:\list.csv" -smtpServer "relay.email.com"
+
+    #>
+    Param(
+        [Parameter(Mandatory = $true, HelpMessage = "Reciever E-mail")]
+        [string]$To,
+        [Parameter(Mandatory = $true, HelpMessage = "Sender E-mail")]
+        [string]$From,
+        [Parameter(Mandatory = $true, HelpMessage = "E-mail Subject")]
+        [string]$Subject,
+        [Parameter(Mandatory = $true, HelpMessage = "E-Mail body")]
+        [string]$Body,
+        [Parameter(Mandatory = $false, HelpMessage = "Full path to attachment")]
+        [string]$Attachment,
+        [Parameter(Mandatory = $true, HelpMessage = "SMTP server")]
+        [string]$smtpServer
+    )
+
+    If ( $Attachment ) {
+        $att = New-Object Net.Mail.Attachment( $Attachment )
+    }
+    $smtp = New-Object Net.Mail.SmtpClient( $smtpServer )
+    $msg            = New-Object Net.Mail.MailMessage
+    $msg.From       = $From
+    $msg.To.Add( $To )
+    $msg.Subject    = $Subject
+    $msg.IsBodyHtml = 1
+    $msg.Body       = $Body
+    If ( $Attachment ) {
+        $msg.Attachments.Add( $att )
+    }
+    $smtp.Send( $msg )
+}
+
+
+Function Send-EMailNew{
+    <#
+    .NOTES
+        ===========================================================================
+        Created by:    Anders Mikkelsen
+        Twitter:       @AMikkelsenDK
+        Creation Date: 2024-04-24
+
+        Based on: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/send-mailmessage
+        ===========================================================================
+    .DESCRIPTION
+        This function sendsan e-mail
+    .PARAMETER To
+        Reciever E-mail
+    .PARAMETER From
+        Sender E-mail
+    .PARAMETER Subject
+        E-mail Subject
+    .PARAMETER Body
+        E-Mail body
+    .PARAMETER smtpServer
+        SMTP server FQDN or IP
+    .PARAMETER Attachment
+        Full path to attachment
+    .EXAMPLE
+        Send-EMailNew -To "to@email.com" -From "from@email.com" -Subject "HEY" -Body "TEST Mail" -smtpServer "relay.email.com" -Alist.csvttachment "C:\"
     #>
     Param(
         [Parameter(Mandatory = $true, HelpMessage = "Reciever E-mail")]
@@ -31,15 +98,25 @@ Function Send-EMail {
         [Parameter(Mandatory = $true, HelpMessage = "E-Mail body")]
         [string]$Body,
         [Parameter(Mandatory = $true, HelpMessage = "SMTP server")]
-        [string]$smtpServer
+        [string]$smtpServer,
+        [Parameter(Mandatory = $true, HelpMessage = "Path to attachment")]
+        [string]$Attachment
     )
 
-    $smtp = new-object Net.Mail.SmtpClient($smtpServer)
-    $msg            = new-object Net.Mail.MailMessage
-    $msg.From       = $From
-    $msg.To.Add($To)
-    $msg.Subject    = $Subject
-    $msg.IsBodyHtml = 1
-    $msg.Body       = $Body
-    $smtp.Send($msg)
+    ## Define the Send-MailMessage parameters
+    $mailParams = @{
+        SmtpServer                 = $smtpServer
+        Port                       = "25"
+        UseSSL                     = $true
+        From                       = $From
+        To                         = $To
+        Subject                    = $Subject
+        Body                       = $Body
+        BodyAsHtml                 = $true
+        Attachment                 = $Attachment
+        DeliveryNotificationOption = "OnFailure", "OnSuccess"
+    }
+
+    ## Send the message
+    Send-MailMessage @mailParams
 }
